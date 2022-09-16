cd148a147051142e4b6a6f5e8840e75e30754051
Lostceleste <inymtern_cl@126.com>
2022-09-16 23:56:49 +0800

    first commit ~

---
+++ src/main/java/com/example/jktest/JkTestApplication.java
@@ -0,0 +1,14 @@
+package com.example.jktest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JkTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JkTestApplication.class, args);
+    }
+
+
+}
---
+++ src/test/java/com/example/jktest/JkTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jktest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JkTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
